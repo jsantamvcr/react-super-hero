3597848f844755f27c8d74866976e79fecda410e
Juan Carlos Santamaría Villegas <43381558+jsantamvcr@users.noreply.github.com>
2025-07-16 20:33:19 -0600

    UPDATE ROUTER

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const HeroCard = ({
+                             id,
+                             superhero,
+                             publisher,
+                             alter_ego,
+                             first_appearance,
+                             characters,
+                         }) => {
+
+    const heroImageUrl = `/assets/heroes/${id}.jpg`;
+    const charactersByHero = (<p>{characters}</p>)
+
+    return (
+        <div className="col">
+            <div className="card">
+                <img src={heroImageUrl} className="card-img-top" alt={superhero}/>
+                <div className="card-body">
+                    <h5 className="card-title">{superhero}</h5>
+                    <p className="card-text">Publisher: {publisher}</p>
+                    <p className="card-text">Alter Ego: {alter_ego}</p>
+                    <p className="card-text">
+                        <div className='text-muted'>First Appearance: {first_appearance}</div>
+                    </p>
+                    {
+                        (alter_ego !== characters) && charactersByHero
+                    }
+
+                    {/*Link to more details:*/}
+                    <Link to={`/heroes/${id}`}>
+                        <button className="btn btn-primary">
+                            More details
+                        </button>
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+    );
+};
