44cd0dabdaee25faec9bd17945d2cdb7a33163d0
Juan Carlos Santamaría Villegas <43381558+jsantamvcr@users.noreply.github.com>
2025-07-16 19:41:20 -0600

    UPDATE ROUTER

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const onLogin = () => {
+        console.log('User logged in');
+        navigate('/marvel', {
+            replace: true
+        });
+    }
+
     return (
         <>
-            <h1>Login page</h1>
+            <div className="container mt-5">
+                <h1 className="text-center">Login Page</h1>
+                <hr/>
+
+                <button className="btn btn-primary"
+                        onClick={onLogin}
+                >Login
+
+                </button>
+
+            </div>
         </>
     );
 };
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true,
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark w-100 p-2 rounded">
 
@@ -37,7 +46,9 @@ export const Navbar = () => {
                         Juan Carlos
                     </span>
 
-                    <button className="nav-item nav-link btn">
+                    <button className="nav-item nav-link btn"
+                        onClick={onLogout}
+                    >
                         Logout
                     </button>
                 </ul>
